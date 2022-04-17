730d07fc5ce27fb157271aeba1a93a6517682371
OzanKaracanta <101042596+OzanKaracanta@users.noreply.github.com>
2022-04-17 14:01:11 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+        fontFamily: {
+            'montserrat': ['Montserrat'],
+            'lato': ['Lato'],
+            'garamond': ['Garamond']
+        }
+    }
+},
+variants: {},
+  plugins: [],
+}
